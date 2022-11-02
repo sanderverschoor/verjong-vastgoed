703ba2546fee71638df652d6c661b8008f864f05
Sander Verschoor <sanderverschoor1909@gmail.com>
2022-11-02 12:01:47 +0100

    Added a user seeder with prefilled credentials and created a model with a controller and a migration

---
+++ database/migrations/2022_11_02_101624_create_houses_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('houses', function (Blueprint $table) {
+            $table->id();
+            $table->string('building_name')->nullable();
+            $table->string('street_name');
+            $table->string('house_number');
+            $table->string('house_number_addition')->nullable();
+            $table->string('postal_code');
+            $table->string('city');
+            $table->string('province');;
+            $table->boolean('smoking_allowed')->default(false);
+            $table->boolean('pets_allowed')->default(false);
+            $table->boolean('furnished')->default(false);
+            $table->boolean('garden')->default(false);
+            $table->boolean('balcony')->default(false);
+            $table->text('description');
+            $table->integer('rent');
+            $table->integer('floors');
+            $table->integer('rooms');
+            $table->text('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('houses');
+    }
+};
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Christien Evers',
+            'email' => 'info@verjongvastgoed.nl' ,
+            'password' => Hash::make('Xs4Verjong!1'),
+        ]);
+    }
+}
